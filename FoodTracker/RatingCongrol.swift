025687a89d03cf3431ced5c6ec58011ad1243f2a
jk <dakoma@gmail.com>
2017-08-05 16:02:08 +0100

    rating

---
+++ FoodTracker/RatingCongrol.swift
@@ -0,0 +1,40 @@
+//
+//  RatingCongrol.swift
+//  FoodTracker
+//
+//  Created by Jakub Korczak on 05/08/2017.
+//  Copyright © 2017 rosecorp. All rights reserved.
+//
+
+import UIKit
+
+class RatingCongrol: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+
+}
